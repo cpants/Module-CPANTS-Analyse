a522e48a5ad5e873fc776a5d112a4ce4abf20972
Kenichi Ishigaki <ishigaki@cpan.org>
2019-08-08 12:58:04 +0900

    release 1.01

--- lib/Module/CPANTS/Analyse.pm
+++ lib/Module/CPANTS/Analyse.pm
@@ -11,7 +11,7 @@ use Archive::Any::Lite;
 use Carp;
 use CPAN::DistnameInfo;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 $VERSION =~ s/_//; ## no critic
 
 __PACKAGE__->mk_accessors(qw(dist opts tarball distdir d mck));
--- lib/Module/CPANTS/Kwalitee/MetaYML.pm
+++ lib/Module/CPANTS/Kwalitee/MetaYML.pm
@@ -7,7 +7,7 @@ use CPAN::Meta::Validator;
 use CPAN::Meta::Converter;
 use List::Util qw/first/;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 $VERSION =~ s/_//; ## no critic
 
 sub order { 10 }
