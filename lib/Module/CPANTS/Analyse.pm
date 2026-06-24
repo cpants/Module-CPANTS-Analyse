dbad793c9cf44d345eb8b33c94e4801a8957c7d6
Kenichi Ishigaki <ishigaki@cpan.org>
2026-06-24 20:14:31 +0900

    Release 1.03

--- lib/Module/CPANTS/Analyse.pm
+++ lib/Module/CPANTS/Analyse.pm
@@ -11,7 +11,7 @@ use Archive::Any::Lite;
 use Carp;
 use Parse::Distname;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 $VERSION =~ s/_//; ## no critic
 
 __PACKAGE__->mk_accessors(qw(dist opts tarball distdir d mck));
